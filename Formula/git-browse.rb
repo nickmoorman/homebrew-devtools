2aaddebb18481ee55f266e5470a2c4f165c41636
Nick Sawyer <nick@nicksawyer.net>
2014-04-17 15:20:06 -0700

    Updated git-browse to 0.1.4

--- Formula/git-browse.rb
+++ Formula/git-browse.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class GitBrowse < Formula
   homepage "https://github.com/nickmoorman/git-browse"
-  url "https://github.com/nickmoorman/git-browse/archive/0.1.3.tar.gz"
-  sha1 "ccfdc1df3a84d27ad9af07bdb33bdcb1aa8a9c9b"
+  url "https://github.com/nickmoorman/git-browse/archive/0.1.4.tar.gz"
+  sha1 "d010eb78f7230199827793a3a465b21ce94b2afd"
 
   def install
     bin.install 'git-browse', 'test-git-browse.py'
